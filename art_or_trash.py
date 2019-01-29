0a56b442ee98008ee53e8d3858b2931ee0ed3158
noamteyssier <nb.teyssier@gmail.com>
2019-01-28 19:10:19 -0800

    added network // loss function // training - working on shape passing

--- art_or_trash.py
+++ art_or_trash.py
@@ -59,6 +59,32 @@ class ArtDataset(Dataset):
         self.df['label'] = self.df.apply(lambda x : self.embed[x['TYPE']], axis = 1)
 
         return self.df
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        # kernel
+        self.conv1 = nn.Conv2d(3, 20, 5)
+        self.conv2 = nn.Conv2d(20, 16, 5)
+
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(829440, 256)
+        self.fc2 = nn.Linear(256, 100)
+        self.fc3 = nn.Linear(100, 10)
+
+        self.pool = nn.MaxPool2d(2,2)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 810 * 32 * 32)
+        # print(x.size())
+        # sys.exit()
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
 
 csv = "data/current_catalog.tab"
 img_dir = "img/train/"
@@ -70,10 +96,53 @@ transform = transforms.Compose([
 art_dataset = ArtDataset(csv, img_dir, transform)
 art_dataset_loader = torch.utils.data.DataLoader(
     dataset=art_dataset,
-    batch_size = 10,
+    batch_size = 10, num_workers = 2,
     shuffle=True
     )
 
-for images,labels in art_dataset_loader:
-    print(labels)
-    break
+
+net = Net()
+# transfer network to gpu
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+net.to(device)
+
+
+########################################
+# Define a Loss Function and Optimizer #
+########################################
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr = 0.001, momentum = 0.9)
+
+#####################
+# Train the Network #
+#####################
+
+for epoch in range(2) :     # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(art_dataset_loader, 0):
+        inputs, labels = [il for il in data]
+
+        # get the inputs
+        inputs, labels = [il.to(device) for il in data]
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print(
+                '[%d, %d] loss: %.3f' %
+                (epoch + 1, i+1, running_loss / 2000)
+                )
+            running_loss = 0.0
+
+print('Finished Training')
